09dab37d040a6313c87d39cc492e6b9fcc3ecf76
roman <roman@MacBook-Air-roman.local>
2024-04-16 10:45:22 +0300

    fix(layout): fixed underline for footer and header component and added padding for main wrapper

--- header.php
+++ header.php
@@ -10,24 +10,27 @@
 </head>
 
 <body>
-    <header class='wrapper header flex'>
-        <a class="link" href="/"><img src="img\luxPM1.jpg" width="230"></a>
-        <div class='header__links'>
-            <a href="luxPM.php" class="link header__link">
-            главная</a>
-            <a href="onas.php" class="link header__link">
-             о нас  </a>
-            <a href="prais.php" class="link header__link">
-            услуги </a>
-            <a href="rabota.php" class="link header__link">
-            наши работы </a>
-            <a href="zapisat.php" class="link header__link">
-            записаться</a>
-            <a href="otziv.php" class="link header__link">
-            отзывы</a>
-            <a href="lk.php" class="link header__link">
-            личный кабинет</a>
+    <header class='wrapper header flex-col'>
+        <div class="flex">
+            <a class="link" href="/"><img src="img\luxPM1.jpg" width="230"></a>
+            <div class='header__links'>
+                <a href="luxPM.php" class="link header__link">
+                главная</a>
+                <a href="onas.php" class="link header__link">
+                о нас  </a>
+                <a href="prais.php" class="link header__link">
+                услуги </a>
+                <a href="rabota.php" class="link header__link">
+                наши работы </a>
+                <a href="zapisat.php" class="link header__link">
+                записаться</a>
+                <a href="otziv.php" class="link header__link">
+                отзывы</a>
+                <a href="lk.php" class="link header__link">
+                личный кабинет</a>
+            </div>
         </div>
+        <div class="line"></div>
     </header>
 </body>
 

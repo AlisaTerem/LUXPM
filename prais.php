9e9c20ac84816cc06bf36d95ac82bd850e1cea67
roman <roman@MacBook-Air-roman.local>
2024-04-16 10:12:48 +0300

    fix(layout): split price table

--- prais.php
+++ prais.php
@@ -5,15 +5,19 @@ $mysli = require __DIR__ . "/database.php";
 require __DIR__ . '/header.php';
 ?>
 <?php
-$sql = sprintf("SELECT * FROM prais_list WHERE mastera_id='1' OR mastera_id='2'");
-$result = $mysli->query($sql);
-$prais_list = $result->fetch_all();
+$sql1 = sprintf("SELECT * FROM prais_list WHERE mastera_id='1'");
+$result1 = $mysli->query($sql1);
+$prais_list1 = $result1->fetch_all();
+
+$sql2 = sprintf("SELECT * FROM prais_list WHERE mastera_id='2'");
+$result2 = $mysli->query($sql2);
+$prais_list2 = $result2->fetch_all();
 ?>
 
 <div class="wrapper price flex-col">
     <h1>Прайс-лист салона LUX PM</h1>
     <div class="flex-col price-block">
-        <h2>Парикмахерские услуги</h2>
+        <h2>Парикмахерские услуги Мастера Алисы</h2>
         <table class="table">
             <thead>
                 <tr>
@@ -23,8 +27,8 @@ $prais_list = $result->fetch_all();
             </thead>
             <tbody>
                 <?php
-                if ($prais_list) {
-                    foreach ($prais_list as $prais) {
+                if ($prais_list1) {
+                    foreach ($prais_list1 as $prais) {
                 ?>
                     <tr>
                         <td class="table__body-row"><?= $prais[1]; ?></td>
@@ -37,6 +41,33 @@ $prais_list = $result->fetch_all();
             </tbody>
         </table>
     </div>
+    <div class="flex-col price-block">
+        <h2>Прайс-лист Мастера Евгении</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th class="table__head-title">Вид услуги</th>
+                    <th class="table__head-title">Стоимость</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                if ($prais_list2) {
+                    foreach ($prais_list2 as $prais) {
+                ?>
+                    <tr>
+                        <td class="table__body-row"><?= $prais[1]; ?></td>
+                        <td class="table__body-row"><?= $prais[4]; ?></td>
+                    </tr>
+                <?php
+                    }
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+
+
 
     <div>
         <div class="grid">
